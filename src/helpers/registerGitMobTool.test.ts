9155184d9397fc04afb2d98649ab8219e944cca5
Mubashwer Salman Khurshid <mubashwersk@gmail.com>
2025-05-28 22:26:41 +1000

    test: add unit test for registerGitMobTool

---
+++ src/helpers/registerGitMobTool.test.ts
@@ -0,0 +1,55 @@
+import { describe, it, expect, jest } from "@jest/globals";
+import { registerGitMobTool } from "./registerGitMobTool";
+import { z, type ZodRawShape } from "zod";
+import type { ToolAnnotations } from "@modelcontextprotocol/sdk/types.js";
+import type {
+  McpServer,
+  ToolCallback,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
+import type { GitMobTool } from "../types/GitMobTool.js";
+
+describe("registerGitMobTool", () => {
+  it("registers given git mob tool with the given server", () => {
+    const mockServer = {
+      registerTool: jest.fn(),
+    } as Partial<McpServer> as McpServer;
+
+    const name = "test_tool";
+    const description = "This is a tool for testing purposes.";
+    const inputSchema: ZodRawShape = {
+      foo: z.string(),
+      bar: z.string(),
+    };
+    const annotations: ToolAnnotations = {
+      title: "Test me",
+      readOnlyHint: false,
+      destructiveHint: false,
+      idempotentHint: false,
+      openWorldHint: false,
+    };
+    const callback: ToolCallback<typeof inputSchema> = async ({ foo, bar }) => {
+      return { content: [{ type: "text", text: foo + bar }] };
+    };
+
+    const testTool: GitMobTool<typeof inputSchema, Record<string, never>> = {
+      name,
+      description,
+      inputSchema,
+      annotations,
+      callback,
+    };
+
+    registerGitMobTool(mockServer, testTool);
+
+    expect(mockServer.registerTool).toHaveBeenCalledWith(
+      testTool.name,
+      {
+        description: testTool.description,
+        inputSchema: testTool.inputSchema,
+        outputSchema: testTool.outputSchema,
+        annotations: testTool.annotations,
+      },
+      testTool.callback,
+    );
+  });
+});
